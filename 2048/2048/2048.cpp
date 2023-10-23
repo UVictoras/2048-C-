3c6d2dc287a67eb9c7c5ffd4f6fbba775b2bc42a
Victor <vmartinan@gaming.tech>
2023-10-23 15:00:24 +0200

    grid created and case fixed

--- 2048/2048/2048.cpp
+++ 2048/2048/2048.cpp
@@ -3,20 +3,40 @@
 
 #include <iostream>
 
-void main()
+class Case
 {
-    std::cout << "Hello World!\n";
-}
-
-class Case {
-public : 
+public:
     bool bIsEmpty;
     int iPositionX, iPositionY;
 
-    Case(int iX, int iY) 
+    Case()
     {
         this->bIsEmpty = true;
-        this->iPositionX = iX;
-        this->iPositionY = iY;
+        this->iPositionX = 0;
+        this->iPositionY = 0;
+    }
+};
+
+class Grid
+{
+public:
+    bool bIsFull;
+    Case cGrid[16];
+
+    Grid()
+    {
+        this->bIsFull = false;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    Grid grid;
+    for (int i = 0; i < 16; i++)
+    {
+        std::cout << grid.cGrid[i].iPositionX;
+        if ((i+1) % 4 == 0)
+            std::cout << "\n";
     }
 }
