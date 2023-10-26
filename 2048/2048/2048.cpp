5e925b460089ce69b5110029ae8bf993a49904f5
Victor <vmartinan@gaming.tech>
2023-10-26 15:00:52 +0200

    fixed the game in general

--- 2048/2048/2048.cpp
+++ 2048/2048/2048.cpp
@@ -18,10 +18,12 @@ class Case
 {
 public:
     int iValue;
+    bool isMerged;
 
     Case()
     {
         this->iValue = 0;
+        this->isMerged = false;
     }
 };
 
@@ -38,13 +40,13 @@ public:
     }
 
     int BiToMono( int x, int y)
-    {
+    {   // Method used to convert monodimensional coordinate to bidimensional coordinate
         int result= x*4 + y;
         return result;
     }
 
     int* MonoToBi(int i)
-    {
+    {   // Method used to convert bidimensional coordinate to monodimensional coordinate
         int* array;
         array[0] = i/4;
         array[1] = i%4;
@@ -58,9 +60,8 @@ public:
     }
 
     void InitiateGrid()
-    {
+    {   // Method from the grid class to iniate the grid's elements as Cases
         cGrid = new Case[16];
-
     }
 
     /*
@@ -74,7 +75,7 @@ public:
     }*/
 
     void PrintGrid()
-    {
+    {   // Method from the grid class which prints the grid in the console log
         int iCaseNb = 0;
         for (int i = 0; i < 9; i++)
         {
@@ -100,7 +101,7 @@ public:
     }
     
     int Move(string sDirection, int iIndex)
-    {
+    { // Method from the grid class used to move the sent tiles to the furthest case to the said direction using recursion
         if (sDirection == "Left")
         {
             if (this->cGrid[iIndex - 1].iValue == 0 && iIndex % 4 != 0)
@@ -162,7 +163,7 @@ public:
     }
 
     void RandNumber() 
-    {
+    { // Modifie la valeur de la case choisis aléatoirement parmis celles vides
         vector<int> iTab = this->ListEmptyCases();
         int iSize = iTab.size();
         int iRandomNumber = iTab[this->Rnd(iSize)];
@@ -175,9 +176,34 @@ public:
         return cGrid[index];
     }
 
-    bool isFull() {
+    bool isFull() 
+    {   // Method from the grid class to check if the grid is full a values, returns true or false
         for (int i = 0; i < 16; i++) {
             if (this->cGrid[i].iValue == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool noPossibility()
+    {   // Method from the grid class to check if there is still a movement possibility in the actual grid, returns true or false
+        for (int l = 0; l < 16; l++)
+        {
+            if (l > 3 && this->cGrid[l].iValue == this->cGrid[l - 4].iValue)
+            {
+                return false;
+            }
+            else if (l < 12 && this->cGrid[l].iValue == this->cGrid[l + 4].iValue)
+            {
+                return false;
+            }
+            else if (l % 4 != 0 && this->cGrid[l].iValue == this->cGrid[l - 1].iValue)
+            {
+                return false;
+            }
+            else if ((l - 3) % 4 != 0 && this->cGrid[l].iValue == this->cGrid[l + 1].iValue)
+            {
                 return false;
             }
         }
@@ -200,102 +226,120 @@ public:
     void GameLoop()
     {
         int iNewIndex, iTemp, iAsciiValue, iScore = 0;
-        char sKey;
+        char sKey; // Initiating our char variable which will be use to get the pressed key
         bool bIsGame = true;
-        while (bIsGame) {
-
-            this->gGameGrid.RandNumber();
+        this->gGameGrid.cGrid[0].iValue = 2;
+        this->gGameGrid.cGrid[1].iValue = 4;
+        this->gGameGrid.cGrid[2].iValue = 2;
+        this->gGameGrid.cGrid[3].iValue = 2;
+        while (bIsGame) 
+        {
+            for (int h = 0; h < 16; h++)
+            {
+                this->gGameGrid.cGrid[h].isMerged = false;
+            }
 
             std::cout << "Score :" << iScore << "\n";
             this->gGameGrid.PrintGrid();
 
-            sKey = _getch(); 
-            iAsciiValue = sKey;
+            if (this->gGameGrid.isFull() == true && this->gGameGrid.noPossibility() == true)  // Checking if the grid is full of tiles and if there is no movement possible, if so we end the game.
+            { 
+                break;
+            }
 
-            if (iAsciiValue == 75)
+            sKey = _getch(); // Function to get the pressed key
+            iAsciiValue = sKey; // Getting the ASCII Value of the key
+
+            if (iAsciiValue == 75) // 75 is the ASCII Value of the left arrow key
             {
-                for (int i = 0; i < 16; i++)
+                for (int i = 0; i < 16; i++) // We go through our array from the top left corner to the bottom right corner
                 {
-                    if (i % 4 != 0 && this->gGameGrid.cGrid[i].iValue != 0)
+                    if (i % 4 != 0 && this->gGameGrid.cGrid[i].iValue != 0) // i%4 = 0 means we're on the left column so we can't move to the left & it's useless to move left if the value is 0
                     {
                         iNewIndex = this->gGameGrid.Move("Left", i);
                         iTemp = this->gGameGrid.cGrid[i].iValue;
                         this->gGameGrid.cGrid[i].iValue = 0;
                         this->gGameGrid.cGrid[iNewIndex].iValue = iTemp;
                         
-                        if (iNewIndex % 4 != 0 && this->gGameGrid.cGrid[iNewIndex - 1].iValue == this->gGameGrid.cGrid[iNewIndex].iValue)
-                        {
+                        if (iNewIndex % 4 != 0 && this->gGameGrid.cGrid[iNewIndex - 1].iValue == this->gGameGrid.cGrid[iNewIndex].iValue && this->gGameGrid.cGrid[iNewIndex - 1].isMerged == false) // We check if the case on the left of the selected one has the same value
+                        { // If that's the case, we merge them and increment the score
                             this->gGameGrid.cGrid[iNewIndex - 1].iValue *= 2;
                             iScore += this->gGameGrid.cGrid[iNewIndex].iValue;
                             this->gGameGrid.cGrid[iNewIndex].iValue = 0;
+                            this->gGameGrid.cGrid[iNewIndex - 1].isMerged = true;
                         }
                     }
                 }
             }
 
-            else if (iAsciiValue == 77)
-            for (int i = 0; i < 16; i++)
+            else if (iAsciiValue == 77) // 77 is the ASCII Value of the right arrow key
             {
-                if ((i-3) % 4 != 0 && this->gGameGrid.cGrid[i].iValue != 0)
+                for (int i = 15; i > -1; i--) // We go through our array from the bottom right corner to the top left corner
                 {
-                    iNewIndex = this->gGameGrid.Move("Right", i);
-                    iTemp = this->gGameGrid.cGrid[i].iValue;
-                    this->gGameGrid.cGrid[i].iValue = 0;
-                    this->gGameGrid.cGrid[iNewIndex].iValue = iTemp;
-                    if ((iNewIndex-3) % 4 != 0 && this->gGameGrid.cGrid[iNewIndex + 1].iValue == this->gGameGrid.cGrid[iNewIndex].iValue)
-                    {
-                        this->gGameGrid.cGrid[iNewIndex + 1].iValue *= 2;
-                        iScore += this->gGameGrid.cGrid[iNewIndex].iValue;
-                        this->gGameGrid.cGrid[iNewIndex].iValue = 0;
+                    if ((i - 3) % 4 != 0 && this->gGameGrid.cGrid[i].iValue != 0) // We substract 3 to our value to have a usable intenger for the % so we can check if we're on the right column
+                    { // If we're not on the right column and the value is not 0 then we can move the case
+                        iNewIndex = this->gGameGrid.Move("Right", i);
+                        iTemp = this->gGameGrid.cGrid[i].iValue;
+                        this->gGameGrid.cGrid[i].iValue = 0;
+                        this->gGameGrid.cGrid[iNewIndex].iValue = iTemp;
+                        if ((iNewIndex - 3) % 4 != 0 && this->gGameGrid.cGrid[iNewIndex + 1].iValue == this->gGameGrid.cGrid[iNewIndex].iValue && this->gGameGrid.cGrid[iNewIndex + 1].isMerged == false) // We check if the case on the right of the selected one has the same value
+                        { // If so, we merge them and increment the score
+                            this->gGameGrid.cGrid[iNewIndex + 1].iValue *= 2;
+                            iScore += this->gGameGrid.cGrid[iNewIndex].iValue;
+                            this->gGameGrid.cGrid[iNewIndex].iValue = 0;
+                            this->gGameGrid.cGrid[iNewIndex + 1].isMerged = true;
+                        }
                     }
                 }
             }
 
-            else if (iAsciiValue == 80)
-            for (int i = 0; i < 16; i++)
+            else if (iAsciiValue == 80) // 80 is the ASCII Value of the down arrow key
             {
-                if (i < 12 && this->gGameGrid.cGrid[i].iValue != 0)
+                for (int i = 15; i > -1; i--) // We go through our array the same way we do for moving right, bottom right to top left
                 {
-                    iNewIndex = this->gGameGrid.Move("Down", i);
-                    iTemp = this->gGameGrid.cGrid[i].iValue;
-                    this->gGameGrid.cGrid[i].iValue = 0;
-                    this->gGameGrid.cGrid[iNewIndex].iValue = iTemp;
-                    if (iNewIndex < 12 && this->gGameGrid.cGrid[iNewIndex + 4].iValue == this->gGameGrid.cGrid[iNewIndex].iValue)
+                    if (i < 12 && this->gGameGrid.cGrid[i].iValue != 0) // If i < 12, this means the value is not the index of a case from the last line, if that's the case and the value is not 0, we can move down the case
                     {
-                        this->gGameGrid.cGrid[iNewIndex + 4].iValue *= 2;
-                        iScore += this->gGameGrid.cGrid[iNewIndex].iValue;
-                        this->gGameGrid.cGrid[iNewIndex].iValue = 0;
+                        iNewIndex = this->gGameGrid.Move("Down", i);
+                        iTemp = this->gGameGrid.cGrid[i].iValue;
+                        this->gGameGrid.cGrid[i].iValue = 0;
+                        this->gGameGrid.cGrid[iNewIndex].iValue = iTemp;
+                        if (iNewIndex < 12 && this->gGameGrid.cGrid[iNewIndex + 4].iValue == this->gGameGrid.cGrid[iNewIndex].iValue && this->gGameGrid.cGrid[iNewIndex + 4].isMerged == false) // We check if the case downward the selected one has the same value
+                        { // If indeed they're equal, we merge them and increment the score
+                            this->gGameGrid.cGrid[iNewIndex + 4].iValue *= 2;
+                            iScore += this->gGameGrid.cGrid[iNewIndex].iValue;
+                            this->gGameGrid.cGrid[iNewIndex].iValue = 0;
+                            this->gGameGrid.cGrid[iNewIndex + 4].isMerged = true;
+                        }
                     }
                 }
             }
 
-            else if (iAsciiValue == 72)
-            for (int i = 0; i < 16; i++)
+
+            else if (iAsciiValue == 72) // 72 is the ASCII Value of the up arrow key
             {
-                if (i >= 4 && this->gGameGrid.cGrid[i].iValue != 0)
+                for (int i = 0; i < 16; i++) // We go through our array the same way we did to go left, from top left to bottom right
                 {
-                    iNewIndex = this->gGameGrid.Move("Up", i);
-                    iTemp = this->gGameGrid.cGrid[i].iValue;
-                    this->gGameGrid.cGrid[i].iValue = 0;
-                    this->gGameGrid.cGrid[iNewIndex].iValue = iTemp;
-                    if (iNewIndex >= 4 && this->gGameGrid.cGrid[iNewIndex - 4].iValue == this->gGameGrid.cGrid[iNewIndex].iValue)
+                    if (i >= 4 && this->gGameGrid.cGrid[i].iValue != 0) // We check if we're not on the 1st line by seeing if the index is at least 4, and if the actual case is also not 0, we can move up
                     {
-                        this->gGameGrid.cGrid[iNewIndex - 4].iValue *= 2;
-                        iScore += this->gGameGrid.cGrid[iNewIndex].iValue;
-                        this->gGameGrid.cGrid[iNewIndex].iValue = 0;
+                        iNewIndex = this->gGameGrid.Move("Up", i);
+                        iTemp = this->gGameGrid.cGrid[i].iValue;
+                        this->gGameGrid.cGrid[i].iValue = 0;
+                        this->gGameGrid.cGrid[iNewIndex].iValue = iTemp;
+                        if (iNewIndex >= 4 && this->gGameGrid.cGrid[iNewIndex - 4].iValue == this->gGameGrid.cGrid[iNewIndex].iValue && this->gGameGrid.cGrid[iNewIndex - 4].isMerged == false) // We look if the upward case has the same value
+                        { // If therefore, the case on top of ours whom value equals the selected one, we mix them and increment the score
+                            this->gGameGrid.cGrid[iNewIndex - 4].iValue *= 2;
+                            iScore += this->gGameGrid.cGrid[iNewIndex].iValue;
+                            this->gGameGrid.cGrid[iNewIndex].iValue = 0;
+                            this->gGameGrid.cGrid[iNewIndex - 4].isMerged = true;
+                        }
                     }
                 }
             }
                       
-            if (iAsciiValue == 27)
+            if (iAsciiValue == 27) // 27 is the ASCII Value of the Escape key, if we press it we end the game
                 bIsGame = false;
 
-            system("cls");
-
-            if (this->gGameGrid.isFull() == true) {
-                break;
-            }
-
+            system("cls"); // Clearing the console logs 
         }
         cout << "You loose";
         //this->gGameGrid.DeleteGrid();
